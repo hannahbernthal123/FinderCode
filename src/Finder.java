1651f5663b6388ba7f5dfcba5ccd23d824995631
hannahbernthal <hannah.bernthal@menloschool.org>
2025-11-08 15:02:01 -0800

    almost done...

--- src/Finder.java
+++ src/Finder.java
@@ -37,32 +37,5 @@ public class Finder {
     }
 
 
-//    public static int hash(String str, int len, int p, keys) {
-//        // Modifies a string to a unique number utilizing hash functions (Horner's method)
-//        int hashed = 0;
-//        for(int i = 0; i < len; i++) {
-//            hashed = (RADIX * hashed + str.charAt(i)) % p;
-//        }
-//        hashed %= p;
-//
-//        // Keep moving to the next item in the array until you find a blank spot, but make sure to wrap if it overflows
-//        if (keys != null) {
-//            while (array[hashed] != null) {
-//                hashed = (hashed + 1) % p;
-//            }
-//        }
-//        return hashed;
-//    }
-//
-//    public static long hash(String s) {
-//        int length = s.length();
-//        long h = 0;
-//        // Preform Horner's Method
-//        for (int i = 0; i < length; i++) {
-//            h = (h * RADIX + s.charAt(i)) % P;
-//        }
-//        return h;
-//    }
-
 
 }
